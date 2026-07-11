b43136a068784e5a62adace9b1564accb3d7477b
Linkina <3498643324@qq.com>
2026-07-11 17:30:31 +0800

    构造好了基础的布局框架

--- src/main.js
+++ src/main.js
@@ -1,31 +1,33 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
+
+<nav>
   <div>
-    <nav>
-      <div>
-        <h1>选项</h1>
-      </div>
-      <div>
-        <h1>xuanxiang</h1>
-      </div>
-    </nav>
+    <h1>选项</h1>
   </div>
-  
-  <span>
-    <div class = "file_area">
+  <div>
+    <h1>xuanxiang</h1>
+  </div>
+</nav>
+
+<div class = "workspace">
+  <aside class = "file_area">
       <h1>test</h1>
-    </div>
-  </span>
-  <span>
-    <div>
-      <h1>text</h1>
-    </div>
-  </span>
-  <span>
-    <div>
-      <h1>test</h1>
-    </div>
-  </span>
+  </aside>
+
+  <main class = "edit_area">
+    <h1>text</h1>
+  </main>
+
+  <section class = "extend_area">
+    <h1>test</h1>
+  </section>
+</div>
+
+ 
+
+   
+
 `
 
